46981f1eb5bea4c4de88a3306e06c9db774d6e64
Laura <laugain8@gmail.com>
2024-07-07 12:32:41 +0200

    Creación constructor y función agregar noticias . Conexion archivo ts con html ngModel

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { Inoticias } from '../../interfaces/inoticias.interface';
+import { FormsModule } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
   arrNoticias: Inoticias [] = []
-  newNoticias: Inoticias = {
+  newNoticia: Inoticias = {
     title: "",
     image: "",
     notice: "",
@@ -21,5 +23,16 @@ export class BlogComponent {
     this.arrNoticias = [
       { title: "Primera Noticia", image: "url blabla", notice: "noticia 1", date: "04-05-2024"},
       { title: "Segunda Noticia", image: "url blabla", notice: "noticia 2", date: "10-05-2024"}
-    ]}
+    ]
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  agregarNoticia(): void {
+    this.arrNoticias.push({...this.newNoticia})
+    this.newNoticia = {title: "", image: "", notice: "", date: ""}
+    
+  }
 }
