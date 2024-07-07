2896c0c269b3d513e7a66a80723fbf46de43681a
Laura <laugain8@gmail.com>
2024-07-07 17:30:37 +0200

    Mejora maquetación y fin

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -21,8 +21,8 @@ export class BlogComponent {
 
   constructor() {
     this.arrNoticias = [
-      { title: "Las mejores playas de España", image: "images/noticia_1.jpg", notice: "Refrescate este verano en la mejor playa de España. Las altas temperaturas han llegado para quedarse. La ola de calor que asola gran parte del país nos tiene recluidos en lugares con ventilador, aire acondicionado o buscando las sombras cuando es irremediable salir a la calle. No obstante, el calor no tiene por qué frenar nuestro ocio, refrescate en esta playa de Cádiz y pasa unos días inolvidables", date: "04-07-2024"},
-      { title: "Concurso de tortillas", image: "images/noticia_2.jpg", notice: "Si te gusta cocinar, no te quedes sin participar en el concurso de tortillas que tendrá lugar en Madrid el próximo 20 de agosto en el Restaurante Pepe. Puedes ganar una comida para dos en el Restaurante Pepe", date: "07-07-2024"}
+      { title: "Las mejores playas de España", image: "images/noticia_1.jpg", notice: "Refrescate este verano en la mejor playa de España. Las altas temperaturas han llegado para quedarse. La ola de calor que asola gran parte del país nos tiene recluidos en lugares con ventilador, aire acondicionado o buscando las sombras cuando es irremediable salir a la calle. No obstante, el calor no tiene por qué frenar nuestro ocio, refrescate en esta playa de Cádiz y pasa unos días inolvidables.", date: "04-07-2024"},
+      { title: "Concurso de tortillas", image: "images/noticia_2.jpg", notice: "Si te gusta cocinar, no te quedes sin participar en el concurso de tortillas que tendrá lugar en Madrid el próximo 20 de agosto en el Restaurante Pepe. Puedes ganar una comida para dos en el Restaurante Pepe.", date: "07-07-2024"}
     ]
   }
 
